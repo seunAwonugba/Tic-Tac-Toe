c0580af746b3744dcd166a98d3e36fbcec75af2b
gentlekboy <gentlekboy@gmail.com>
2021-08-21 00:11:14 +0100

    Created development branch

--- app/src/main/java/com/gentlekboy/tictactoe/GameModeActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/GameModeActivity.kt
@@ -2,6 +2,7 @@ package com.gentlekboy.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 //This activity allows users choose if they want to play with the computer or with a friend
 class GameModeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/gentlekboy/tictactoe/MainMenuActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/MainMenuActivity.kt
@@ -2,10 +2,9 @@ package com.gentlekboy.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-/*
-This activity serves as the game's main menu
-It contains navigation links to play the game, rate the app, share the app, report a bug and exit the app
- */
+
+//This activity serves as the game's main menu
+//It contains navigation links to play the game, rate the app, share the app, report a bug and exit the app
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
