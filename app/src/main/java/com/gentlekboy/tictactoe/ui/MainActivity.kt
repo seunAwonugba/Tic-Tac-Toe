9978e6c4e2006a5395a239d3bd945873d12deab9
seunawonugba <seun.awonugba@decagon.dev>
2021-08-21 15:11:08 +0100

    delayed navigation to the next page by 2 seconds

--- app/src/main/java/com/gentlekboy/tictactoe/ui/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/tictactoe/ui/MainActivity.kt
@@ -1,12 +1,20 @@
-package com.gentlekboy.tictactoe
+package com.gentlekboy.tictactoe.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.gentlekboy.tictactoe.R
 
 //This activity holds a splash screen that welcomes users to the app
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent = Intent(this, LandingPageActivity::class.java)
+            startActivity(intent)
+        }, 1500)
     }
 }
